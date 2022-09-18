7ee8358b5f5e7f7c73e5acf0669370d7864d4423
klstxii <ksarmiento@student.hau.edu.ph>
2022-09-18 15:53:32 +0800

    initial commit

---
+++ tothepurpleline/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import ContactForm
+
+def home_page(request):
+    return render(request, "home_page.html")
+
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+
+    context = {
+        'form': contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request, "contact/contact.html", context)
