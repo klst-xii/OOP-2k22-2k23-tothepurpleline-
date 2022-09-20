dc2c053e72f4eced70424836b0f8f1115cd0dff5
sddizon1 <113536075+sddizon1@users.noreply.github.com>
2022-09-20 17:09:31 +0800

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import CreateView
+from django.contrib.auth import login
+from accounts.forms import RegisterForm, LoginForm
 
-from accounts.forms import RegisterForm
 
+def login_page(request):
+    form = LoginForm(request.POST or None)
+
+    if request.POST and form.is_valid():
+        user = form.login(request)
+        print(user)
+        if user:
+            print(user)
+            login(request, user)
+            return redirect('home')
+    context = {
+        "form": form
+    }
+    return render(request, 'auth/login.html', context)
 
 class RegisterView(CreateView):
     form_class = RegisterForm
