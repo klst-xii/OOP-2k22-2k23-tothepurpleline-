3cda66bd33d05bf5009a2362a3df593c7c301e1d
sddizon1 <113536075+sddizon1@users.noreply.github.com>
2022-09-20 17:10:34 +0800

    Update urls.py

--- tothepurpleline/urls.py
+++ tothepurpleline/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_page, contact_page
-from accounts.views import RegisterView
+from accounts.views import RegisterView, login_page
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', home_page),
+    path('', home_page, name='home'),
     path('contact/', contact_page),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="auth/logout.html"), name='logout'),
+    path('login', login_page, name='login'),
     path('admin/', admin.site.urls),
 ]
