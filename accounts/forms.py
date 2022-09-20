c94257c353d59c0b992cf00c1824c49f1003bc1f
klstxii <ksarmiento@student.hau.edu.ph>
2022-09-20 11:58:41 +0800

    initial commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -34,12 +34,12 @@ class RegisterForm(forms.ModelForm):
             password2 = self.cleaned_data.get('password2')
 
             if password2 != password1:
-                raise forms.ValidationError("Password doesnot match")
+                raise forms.ValidationError("Password does not match")
             return password2
 
         def save(self, commit=True):
             user = super(RegisterForm, self).save(commit=False)
-            user.set_password(self.clean_password2["password1"])
+            user.set_password(self.cleaned_data["password1"])
 
             if commit:
                 user.save()
