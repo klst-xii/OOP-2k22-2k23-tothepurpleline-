13aa75823c11fbff4bb14c7dc3393ca24d72150d
klstxii <ksarmiento@student.hau.edu.ph>
2022-09-26 23:17:34 +0800

    initial commit

--- products/models.py
+++ products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.db.models import Model
 
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=10.00)
+    image = models.FileField(upload_to='products/', null=True, blank=True)
+
 
     def __str__(self):
         return self.title
