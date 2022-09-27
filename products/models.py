a680c7ba5732cc3e574345500a8854296e35d83e
klstxii <ksarmiento@student.hau.edu.ph>
2022-09-27 12:29:42 +0800

    initial commit

--- products/models.py
+++ products/models.py
@@ -1,12 +1,26 @@
+import random
+import os
+
 from django.db import models
 from django.db.models import Model
 
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 2541781232)
+    name, ext = get_filename_ext(filename)
+    final_name = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_name}".format(new_filename=new_filename, final_name=final_name)
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=10.00)
-    image = models.FileField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
 
     def __str__(self):
--- tothepurpleline/urls.py
+++ tothepurpleline/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from .views import home_page, contact_page
 from accounts.views import RegisterView, login_page
 from django.contrib.auth import views as auth_views
-# from products.views import ProductListView
-# from products.views import ProductDetailView
+from products.views import ProductListView
+from products.views import ProductDetailView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -18,8 +18,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name="auth/logout.html"), name='logout'),
     path('login', login_page, name='login'),
     path('admin/', admin.site.urls),
-    # path('products/', ProductListView.as_view()),
-    # path('products/<int:pk>', ProductDetailView.as_view(), name='detailed'),
+    path('products/', ProductListView.as_view()),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='detailed'),
 ]
 
 if settings.DEBUG:
