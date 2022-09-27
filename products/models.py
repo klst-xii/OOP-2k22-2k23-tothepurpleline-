11d8bc6b3da2ccb7347528c91da309fdef07b7e5
DeanIrish <113547348+DeanIrish@users.noreply.github.com>
2022-09-27 13:24:46 +0800

    Update models.py

--- products/models.py
+++ products/models.py
@@ -16,7 +16,23 @@ def upload_image_path(instance, filename):
     final_name = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
     return "products/{new_filename}/{final_name}".format(new_filename=new_filename, final_name=final_name)
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+    
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
 class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+    
+    def all(self):
+        return self.get_queryset().active()
+    
+    def featured(self):
+        return self. get_queryset().featured()
+    
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
@@ -28,6 +44,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=10.00)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
     
     objects = ProductManager()
 
