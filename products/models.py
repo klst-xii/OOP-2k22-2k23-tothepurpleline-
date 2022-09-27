309fb099b7e1d4891c36dbbf5e146c709b1d8422
DeanIrish <113547348+DeanIrish@users.noreply.github.com>
2022-09-27 21:24:03 +0800

    Update models.py

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=10.00)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
-    
+
 
     def __str__(self):
         return self.title
