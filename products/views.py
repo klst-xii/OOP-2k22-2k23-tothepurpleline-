95df3a003fba36bfb8d8e8bd1c9966add868229d
DeanIrish <113547348+DeanIrish@users.noreply.github.com>
2022-09-27 21:10:22 +0800

    Update views.py

--- products/views.py
+++ products/views.py
@@ -5,23 +5,11 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 
 
-class ProductFeaturedListView(ListView):
+class ProductListView(ListView):
+    queryset = Product.objects.all()
     template_name = "product/list.html"
     
-    def get_queryset(seld, *args, **kwargs):
-        request = self.request
-        return Product.objects.all().featured()
-
-class ProductFeaturedDetailView(DetailView):
-    queryset = Product.objects.all().featured()
+class ProductDetailView(DetailView):
+     queryset = Product.objects.all()
      template_name = "products/detail.html"
         
-     def get_context_data(self, *args, **kwargs):
-        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        return context
-    
-    def get_queryset(self, *args, **kwargs):
-        request = self.request
-        pk = self.kwargs.get('pk')
-        return Product.objects.filter(pk=pk)
-
