817c0669a40f9ab67c23e0e85517bde41f6397d2
DeanIrish <113547348+DeanIrish@users.noreply.github.com>
2022-09-28 00:12:31 +0800

    Update urls.py

--- tothepurpleline/urls.py
+++ tothepurpleline/urls.py
@@ -20,7 +20,8 @@ urlpatterns = [
     path('featured/', ProductFeaturedListView.as_view(), name='feature_list'),
     path('products/', ProductListView.as_view(), name='product_list'),
     path('featured/<int:pk>', ProductFeaturedDetailView.as_view(), name='featured_detailed'),
-    path('products/<int:pk>', ProductDetailView.as_view(), name='detailed'),
+    #path('products/<int:pk>', ProductDetailView.as_view(), name='detailed'),
+    re_path(r'^products/(?<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     path('delete/<int:id>', delete, name='delete'),
     path('update/udaterecord/<int:id>' updaterecord, name='updaterecord')
     path('logout/', auth_views.LogoutView.as_view(template_name="auth/logout.html"), name='logout'),
