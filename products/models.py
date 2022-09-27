5406bc5efc519219d4df240d7b9b852c02a96438
DeanIrish <113547348+DeanIrish@users.noreply.github.com>
2022-09-27 13:10:43 +0800

    Update models.py

--- products/models.py
+++ products/models.py
@@ -16,12 +16,20 @@ def upload_image_path(instance, filename):
     final_name = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
     return "products/{new_filename}/{final_name}".format(new_filename=new_filename, final_name=final_name)
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=10.00)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
-
+    
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
