cc3aead1547bd527587c92b9e800674fc53283c6
klstxii <ksarmiento@student.hau.edu.ph>
2022-11-01 13:10:21 +0800

    initial commit

--- tothepurpleline/urls.py
+++ tothepurpleline/urls.py
@@ -10,7 +10,7 @@ from products.views import ProductDetailView
 from products.views import ProductFeaturedListView
 from products.views import ProductFeaturedDetailView
 from products.views import ProductDetailSlugView
-from socials.views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower
+from socials.views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -33,10 +33,13 @@ urlpatterns = [
     path('socials/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
     path('socials/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('socials/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('socials/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('socials/<int:pk>/dislike', AddDislike.as_view(), name='dislike'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
     path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+
     path("__reload__/", include('django_browser_reload.urls')),
 
 ]
